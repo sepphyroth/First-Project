98bcfef90d1f0545fc3db6103b219bce91031fde
Anderson Bueno <anderson.buenop@gmail.com>
2020-06-20 22:00:16 -0300

    Testar a remoção da msg de console

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 'use strict';
-console.log('Here \'s a hidden menssage');
+
 
 let today = new Date();
 let formatDate = today.toDateString();
 let selectElement =  document.getElementById('date');
 selectElement.innerHTML = formatDate;
+
+console.log('Here \'s a hidden menssage');
